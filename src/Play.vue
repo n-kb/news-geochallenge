e385d2c3a3b6e6170f59259fef874a95662c1ae0
Nicolas Kayser-Bril <hi@nkb.fr>
2018-07-11 23:09:08 +0200

    added loader

--- src/Play.vue
+++ src/Play.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <div class="modal" :class="{'is-active': isLoading}">
+      <div class="modal-background"></div>
+      <div class="modal-content has-text-centered">
+       <p class="white">
+        Loading...
+       </p>
+      </div>
+      <button class="modal-close is-large" aria-label="close"></button>
+    </div>
     <div class="columns is-centered content is-fullheight no-overflow">
       <div class="column">
         <quizmap :questions="questions" :difficulty="difficulty" :token="token"></quizmap>
@@ -17,7 +26,8 @@ export default {
   data () {
     return {
       questions: [],
-      token: ""
+      token: "",
+      isLoading: true
     }
   },
   computed: {
@@ -27,15 +37,20 @@ export default {
   },
   created () {
     window.instance.get("get_questions/" + this.difficulty).then(response => {
-          this.token = response.data.token;
-          this.questions = response.data.questions;
-        });
+      this.token = response.data.token;
+      this.questions = response.data.questions;
+      this.isLoading = false;
+    });
   },
   components: { quizmap }
 }
 </script>
 
 <style lang="sass">
+
+.white
+  color: #fff
+
 .no-overflow
   overflow: hidden
 </style>
